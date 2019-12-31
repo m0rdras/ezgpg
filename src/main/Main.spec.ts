b1b33be4b13dc302b4c65ad05dddc5942bdfd288
m0rdras <m0rdras@users.noreply.github.com>
2019-12-31 15:49:54 +0100

    added test for Main.onRequestPubKey

--- src/main/Main.spec.ts
+++ src/main/Main.spec.ts
@@ -1,6 +1,7 @@
 import { ipcMain, IpcMain, IpcMainEvent } from 'electron';
 
 import { Events } from '../Constants';
+import { GpgError } from './Gpg';
 import Main from './Main';
 
 type OnMockFn = (
@@ -18,5 +19,37 @@ describe('Main', () => {
         expect(onMock.calls[0][1]).toBeInstanceOf(Function);
         expect(onMock.calls[1][0]).toEqual(Events.CRYPT);
         expect(onMock.calls[1][1]).toBeInstanceOf(Function);
+    });
+
+    describe('onRequestPubKeys', () => {
+        it('handles request for public keys', async () => {
+            const mockGpg = { getPublicKeys: jest.fn() } as any;
+            mockGpg.getPublicKeys.mockImplementation(async () => {
+                return ['alpha', 'beta'];
+            });
+            const mockEvent = { reply: jest.fn() } as any;
+            const main = new Main(mockGpg);
+            await main.onRequestPubKeys(mockEvent);
+            expect(mockEvent.reply.mock.calls).toHaveLength(1);
+            expect(mockEvent.reply.mock.calls[0]).toEqual([
+                Events.PUBKEYS_RESULT,
+                ['alpha', 'beta']
+            ]);
+        });
+
+        it('handles request for public keys when gpg errors', async () => {
+            const mockGpg = { getPublicKeys: jest.fn() } as any;
+            mockGpg.getPublicKeys.mockImplementation(async () => {
+                throw new GpgError(2, 'error');
+            });
+            const mockEvent = { reply: jest.fn() } as any;
+            const main = new Main(mockGpg);
+            await main.onRequestPubKeys(mockEvent);
+            expect(mockEvent.reply.mock.calls).toHaveLength(1);
+            expect(mockEvent.reply.mock.calls[0]).toEqual([
+                Events.PUBKEYS_RESULT,
+                []
+            ]);
+        });
     });
 });
