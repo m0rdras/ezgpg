4e5a518b19bf6800677479de7c71a3b6b7945be4
m0rdras <m0rdras@users.noreply.github.com>
2020-01-09 15:08:45 +0100

    await createWindow promise result on startup

--- src/index.ts
+++ src/index.ts
@@ -56,11 +56,11 @@ app.on('window-all-closed', () => {
     }
 });
 
-app.on('activate', () => {
+app.on('activate', async () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow();
+        await createWindow();
     }
 });
 
