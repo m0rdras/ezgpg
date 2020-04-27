2fc717345d7f36dd5f2031b091f6ec79150100b8
m0rdras <m0rdras@users.noreply.github.com>
2020-04-27 14:54:51 +0200

    updated ForkTsCheckerWebpackPlugin options to use ESLint

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = [
-    new ForkTsCheckerWebpackPlugin({
-        async: false
-    })
-];
+const forkTsCheckerOptions = {
+    async: false,
+    eslint: true
+};
+
+module.exports = [new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions)];
